baf5d4f7ccb4fe3f057d7ac4edf183426ff96c24
rod <rodrip0129@gmail.com>
2023-10-17 12:58:34 -0300

    add models, migration and controller to Recipe

---
+++ db/migrate/20231017154400_create_recipes.rb
@@ -0,0 +1,9 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_017_152_155) do
+ActiveRecord::Schema[7.0].define(version: 20_231_017_154_400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 20_231_017_152_155) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['user_id'], name: 'index_foods_on_user_id'
+  end
+
+  create_table 'recipes', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_recipes_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -39,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 20_231_017_152_155) do
   end
 
   add_foreign_key 'foods', 'users'
+  add_foreign_key 'recipes', 'users'
 end
