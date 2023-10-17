f1cffa63b63761f78e9f8547bd16c952b6944d0b
rod <rodrip0129@gmail.com>
2023-10-17 12:29:32 -0300

    add models, migration and controller to Food

---
+++ db/migrate/20231017152155_create_foods.rb
@@ -0,0 +1,13 @@
+class CreateFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.integer :quantity
+      t.decimal :price
+      t.string :measurement_unit
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_016_232_139) do
+ActiveRecord::Schema[7.0].define(version: 20_231_017_152_155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'foods', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'quantity'
+    t.decimal 'price'
+    t.string 'measurement_unit'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_foods_on_user_id'
+  end
 
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
@@ -26,4 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 20_231_016_232_139) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+
+  add_foreign_key 'foods', 'users'
 end
