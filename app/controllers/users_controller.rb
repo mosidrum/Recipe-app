015428d7919408315b78c6c9197ea10752bb11ed
rod <rodrip0129@gmail.com>
2023-10-18 07:56:41 -0300

    fix issue with login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
 
   def index
     @users = User.all
   end
 
+
   def show
-    if params[:id] = 'sign_out'
+    if params[:id] == 'sign_out' # Cambié '=' por '=='
       sign_out_redirect_to
     else
       find_user
+    end
   end
 
   def find_user
-    @user = USer.find_by(id:, params[:id])
-    redirect_to_user_path, alert: 'User not found' unless @user
+    @user = User.find_by(id: params[:id]) # Corregí el formato de los parámetros
+    redirect_to user_path, alert: 'User not found' unless @user # Corregí la ruta y el formato del 'unless'
   end
 
   def sign_out_redirect_to
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show] do
+  end
+
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'users#index', as: :unauthenticated_root
+    end
+  end
 end
