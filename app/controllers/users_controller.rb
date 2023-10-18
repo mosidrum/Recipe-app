4b393c4cfdc318fe9e9eda7cedbc80dc423e38b9
mosidrum <mosiokanga@gmail.com>
2023-10-18 11:25:17 +0100

    Add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    if params[:id] = 'sign_out'
+      sign_out_redirect_to
+    else
+      find_user
+  end
+
+  def find_user
+    @user = USer.find_by(id:, params[:id])
+    redirect_to_user_path, alert: 'User not found' unless @user
+  end
+
+  def sign_out_redirect_to
+    sign_out current_user
+    redirect_to new_user_session_path
+  end
 end
