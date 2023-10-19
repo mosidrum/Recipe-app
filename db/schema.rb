d0ca6ddfe9f13a5e59fee1a43448f746ad651fe4
rod <rodrip0129@gmail.com>
2023-10-19 06:41:12 -0300

    delete unnecesary migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_073448) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_18_142520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
-    t.float "price"
+    t.decimal "price"
     t.string "measurement_unit"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_073448) do
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.integer "quantity"
     t.bigint "recipe_id", null: false
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
@@ -36,21 +35,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_073448) do
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.decimal "preparation_time"
-    t.decimal "cooking_time"
-    t.text "description"
-    t.boolean "public"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "cooking_time"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
