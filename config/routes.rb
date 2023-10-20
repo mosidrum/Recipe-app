c8bdf324c5b0e1409488d9f37fb22e3599ed56f6
rod <rodrip0129@gmail.com>
2023-10-20 11:58:13 -0300

    fix errors to show foods page

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,16 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :recipes do
-    resources :recipes_foods, only: %i[new create destroy]
+    resources :recipe_foods, only: %i[new create destroy]
   end
 
   resources :foods, only: %i[index show new create destroy]
   resources :public_recipes, only: [:index]
-
-  # get 'public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
-
   resources :shopping_lists, only: [:index] do
     collection do
       get 'generate'
     end
   end
+
+  get 'public_recipes_custom', to: 'recipes#public_recipes', as: 'public_recipes_custom'
 end
