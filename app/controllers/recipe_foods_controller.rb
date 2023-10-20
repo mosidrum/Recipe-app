9eae148444498e791d1f19dba264d697331fb5c4
mosidrum <mosiokanga@gmail.com>
2023-10-20 03:38:06 +0100

    Add controllers and forms for food and recipes

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,2 +1,52 @@
 class RecipeFoodsController < ApplicationController
+  before_action :find_recipe
+  before_action :find_recipe_food, only: %i[edit update destroy]
+
+  def new
+    @recipe_food = RecipeFood.new
+    @foods = Food.all
+  end
+
+  def edit
+    @foods = Food.all
+  end
+
+  def update
+    if @recipe_food.update(recipe_food_params)
+      redirect_to recipe_path(@recipe), notice: 'Successfully Updted Ingredient.'
+    else
+      @foods = Food.all
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe_food.destroy
+    redirect_to recipe_path(@recipe), notice: 'Ingredient deleted successfully.'
+  end
+
+  def create
+    @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
+    if @recipe_food.save
+      redirect_to recipe_path(@recipe), notice: 'Successfully Added Ingredient.'
+    else
+      @foods = Food.all
+      render :new
+    end
+  end
+
+  private
+
+  def find_recipe_food
+    @recipe_food = @recipe.recipe_foods.find(params[:id])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :quantity)
+  end
+
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,41 @@
 class RecipesController < ApplicationController
+  before_action :set_user
+
+  def index
+    @recipes = @user.recipes.includes(:user) if user_signed_in?
+  end
+
+  def destroy
+    @recipe = @user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Successfully Removed Recipe.'
+  end
+
+  def create
+    @recipes = user.recipes.build(recipes_Params)
+    if @recipes.save
+      redirect_to recipes_path
+    else
+      reder :new
+    end
+  end
+
+  def new
+    @recipe = @user.recipes.build
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods.includes(:food)
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :public, :description)
+  end
 end
